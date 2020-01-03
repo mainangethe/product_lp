1758bba18124034bacfa1b588937c9813b4e019d
Ng'ethe Maina <ngethe@mradi.co.ke>
2020-01-03 17:12:54 +0300

    Add responsiveness
    Allow site to respond dynamically to different device screen sizes.

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -3,3 +3,15 @@
   Date: Jan 3, 2020
   Topic: Responsive Shoe landing page scripts
 */
+
+$(function () {
+  $('.menu-toggle, .fa-times').on('click', function() {
+    $('nav').toggleClass('active');
+    $('.overlay').toggleClass('menu-open');
+  });
+
+  $('.overlay').on('click', function () {
+  $('nav').removeClass('active');
+  $(this).removeClass('menu-open');
+  });
+});
